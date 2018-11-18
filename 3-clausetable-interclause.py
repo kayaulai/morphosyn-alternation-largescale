00f899d28a096e7d11a339e6a127d64af922a84d
kayaulai <kayaulai@connect.hku.hk>
2018-11-18 22:21:49 +0800

    Add code for extracting coreference data and adding it to table
    Coref chains are stored in the table; the features will be derived from these in R

--- 3-clausetable-interclause.py
+++ 3-clausetable-interclause.py
@@ -38,13 +38,91 @@ nltk.download('verbnet');
 from nltk.corpus import verbnet as vn
 import csv
 from nltk.corpus import wordnet as wn
-s = shelve.open("rawdata.dat");
+s = shelve.open("G:\\我的雲端硬碟\\corpus priming\\eng\\rawdata.dat");
 allSents = s["allSents"];
 s.close();
 pd.set_option("max_columns",50)
 from nltk.stem import WordNetLemmatizer
 wnl = WordNetLemmatizer()
-clauseTable = pd.read_csv("sept26table.csv");
+clauseTable = pd.read_csv("G:\\我的雲端硬碟\\corpus priming\\eng\\oct5table.csv");
+
+import spacy;
+nlp = spacy.load('en_core_web_sm')
+
+s2 = shelve.open("G:\\我的雲端硬碟\\corpus priming\\eng\\mentions.dat");
+mentionsList = s2["mentionsList"];
+s2.close();
+
+def addChain(orig, new):
+    if orig != "/":
+        origSplit = orig.split(",");
+        if str(new) not in origSplit:
+            new = orig + "," + new;
+    return(new);
+
+#Actually using the coreference data
+i = 0;
+j = 0;
+k = 0;
+newClauseTable = pd.DataFrame();
+while i + j < len(allSents[0:40]):
+    i = i + j;
+    sentence = allSents[i];
+    j = 1;
+    found = False;
+    currSent = allSents[i+j];
+    currDoc = currSent["doc"];
+    currSents = [allSents[i+j]];
+    
+    currClauses =  clauseTable.loc[clauseTable["Doc"] == currDoc,:];
+    #currCorefTable = pd.DataFrame(columns=["SubjMention-","SubjMention-1","SubjMention-2","SubjMention-3","SubjMention-4","SubjMention-5","SubjMention-6","SubjMention-7","SubjMention-8","SubjMention-9","SubjMention-10","ObjMention1","ObjMention2","ObjMention3","ObjMention4","ObjMention5","ObjMention6","ObjMention7","ObjMention8","ObjMention9","ObjMention10"]);
+    #currCorefTable = pd.DataFrame(["SubjChain","ObjChain","OblChain"])
+    currClauses["SubjChain"] = np.full(currClauses.shape[0],"/");
+    currClauses["ObjChain"] = np.full(currClauses.shape[0],"/");
+    currClauses["OblChain"] = np.full(currClauses.shape[0],"/");
+    
+    while found == False:
+        if (i + j) == len(allSents): found = True; break;
+        currSent = allSents[i+j];
+        if currSent['doc'] != sentence['doc']:
+            found = True;
+        else:
+            currSents.append(allSents[i+j]);
+            j = j + 1;
+            
+            
+    m = 0;
+    currClausesNew = pd.DataFrame();
+    for mention in mentionsList[k]:
+        chainID = str(m);
+        for item in mention[1]:
+              for i in np.arange(0,currClauses.shape[0]-1,1):
+                  if (currClauses.iloc[i,]["SentID"]+1)==item['sentNum']:
+                      print(item);
+                      if currClauses.iloc[i,]["SubjPosID"] != "/":
+                          if item['headIndex'] == int(currClauses.iloc[i,]["SubjPosID"]):
+                              currClauses["SubjChain"][i] = addChain(currClauses["SubjChain"][i],chainID);
+                              print(currClauses["SubjChain"][i])
+                              print(addChain(currClauses["SubjChain"][i],chainID))
+                         
+                      if currClauses.iloc[i,]["ObjPosID"] != "/":
+                          if item['headIndex'] == int(currClauses.iloc[i,]["ObjPosID"]):
+                              currClauses["ObjChain"][i] = addChain(currClauses["ObjChain"][i],chainID);
+                          
+                      if currClauses.iloc[i,]["OblPosID"] != "/":
+                          if item['headIndex'] == int(currClauses.iloc[i,]["OblPosID"]):
+                              currClauses["OblChain"][i] = addChain(currClauses["OblChain"][i],chainID);
+        
+        m = m + 1;
+    
+    newClauseTable = newClauseTable.append(currClauses);
+                
+            
+    k = k + 1;
+    print(j);
+    print(i);
+
+
 
 #Previous k clauses
 k = 10;
